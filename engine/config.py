a1ea0fa8fc36d73fb6defaaf7a2fffa061e9efbc
Iris-Raine <iris.raine.darkest.star@gmail.com>
2024-03-01 00:34:49 +0000

    change avatar static url

--- engine/config.py
+++ engine/config.py
@@ -6,7 +6,7 @@ SENDING_TARGET_TIME = {
 BASIC_COLOR_CODE = (48, 213, 200)
 
 HEADER_BOT_NAME = "Олень Вениамин"
-HEADER_BOT_AVATAR_STATIC_URL = 'https://cdn.discordapp.com/app-icons/1202638012346007552/201559bbe2e520f038909b495a92571d.png?size=256'
+HEADER_BOT_AVATAR_STATIC_URL = 'https://cdn.discordapp.com/app-icons/1202638012346007552/92da98040d269ee6d6ebcc8857df5784.png?size=256'
 
 EVENT_SETTINGS = 'assets/settings.json'
 INTRODUCTION_IMAGE_FILEPATH = 'assets/title_image.png'
